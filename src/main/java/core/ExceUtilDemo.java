814fbd471fc0f5142cac626a1a5885032a974da8
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-14 18:22:38 +0530

    Utility - upgrade
    utility upgrade

--- src/main/java/core/ExceUtilDemo.java
+++ src/main/java/core/ExceUtilDemo.java
@@ -1,9 +1,19 @@
 package core;
 
-public class ExceUtilDemo extends ExcelUtilsExample {
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class ExceUtilDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ExcelUtilsExample eue=new ExcelUtilsExample("UserPassword","Sheet1");
+		LinkedHashMap<String,List<String>> data=eue.getColumnData(0);
+		System.out.println(data);
+		LinkedHashMap<String,List<String>> data1=eue.getColumnData(1);
+		System.out.println(data1);
+		LinkedHashMap<String,List<String>> data2=eue.getColumnData(1);
+		System.out.println(data2);
 		
 	}
 
--- src/main/java/core/ExcelUtilsExample.java
+++ src/main/java/core/ExcelUtilsExample.java
@@ -1,27 +1,41 @@
 package core;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
 
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
 
 public class ExcelUtilsExample {
 	static XSSFWorkbook workbook;
 	static XSSFSheet sheet;
+	 XSSFCell cell;
 	static File file;
 	static String filePath;
 	String workbookName;
 	String sheetName;
-	public void ExcelUtilExample(String workbookname,String sheetname) {
+	LinkedHashMap<String,List<String>> cellData;
+	public ExcelUtilsExample(String workbookname,String sheetname) {
 		this.workbookName=workbookname;
 		this.sheetName=sheetname;
 	}
+	
 	public XSSFWorkbook getWorkbook() {
 		filePath="src/test/resources/"+this.workbookName+".xlsx";
 		try {
 		file=new File(filePath);
 		if(file.exists()) {
-			System.out.println(filePath);
+			//System.out.println(filePath);
 			workbook=new XSSFWorkbook(file);
 			return workbook;
 		}// END OF IF BLOCK
@@ -32,6 +46,16 @@ public class ExcelUtilsExample {
 			System.out.println();
 			ex.getStackTrace();
 		}// END OF CATCH BLOCK
+		finally {
+			file=null;
+			try {
+				workbook.close();
+				workbook=null;
+			}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		}
 		return workbook;
 	}
 	public XSSFSheet getSheet() {
@@ -52,5 +76,46 @@ public class ExcelUtilsExample {
 	public int getColumn() {
 		return getSheet().getRow(0).getPhysicalNumberOfCells();
 	}
-	
+	public LinkedHashMap<String,List<String>> getColumnData(int columnNo){
+		int rows=getRows();
+		cellData=new LinkedHashMap<String,List<String>>();
+		List<String> list=new ArrayList<String>();
+		for(int i=1;i<rows;i++) {
+			cell=getSheet().getRow(i).getCell(columnNo);
+			CellType cellType=cell.getCellType();
+			switch(cellType) {
+			case NUMERIC:
+				if(cell.getNumericCellValue()!=0 && DateUtil.isCellDateFormatted(cell))
+				{
+					DateFormat df=new SimpleDateFormat("dd/MM/yyyy");
+					Date dt=cell.getDateCellValue();
+					String curDate=df.format(dt);
+					list.add(curDate);
+				
+				}
+				else
+					list.add(String.valueOf(cell.getNumericCellValue()));
+				break;
+			case STRING:
+				list.add(cell.getStringCellValue());
+				break;
+			case BOOLEAN:
+				list.add(String.valueOf(cell.getBooleanCellValue()));
+				break;
+			case BLANK:
+				break;
+			case ERROR:
+				break;
+			case FORMULA:
+				break;
+			case _NONE:
+				break;
+			default:
+				break;
+			}
+			
+		}
+		cellData.put(sheet.getRow(0).getCell(0).getStringCellValue(), list);
+		return cellData;
+	}
 }
