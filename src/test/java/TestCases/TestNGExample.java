180a3ceb5545bb7475b7eb9d790bdcd8adcc9f78
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-19 14:11:45 +0530

    Applying mutliple search

--- src/test/java/TestCases/TestNGExample.java
+++ src/test/java/TestCases/TestNGExample.java
@@ -3,6 +3,7 @@ package TestCases;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -11,13 +12,15 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+//import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestNGExample {
 	private WebDriver driver=null;
-	@BeforeClass
+	@BeforeTest
 	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 		driver=new ChromeDriver();
@@ -46,7 +49,33 @@ public class TestNGExample {
 		}
 		
 	}
-	@AfterClass
+	
+	@Test(priority = 2)
+	public void SearchMultiTest() {
+		List<String> data=new ArrayList<String>();
+		data.add("Testng Example");
+		data.add("Cucumber Example");
+		data.add("Unix example");
+		data.add("Cyperious example");
+		driver.get("https://www.amazon.in/");
+		
+		WebElement search=driver.findElement(By.id("twotabsearchtextbox"));
+		for(String s:data) {
+		//search=driver.findElement(By.xpath("//input[@name='q']"));
+			//driver.get("https://www.amazon.in/");
+			
+			search=driver.findElement(By.id("twotabsearchtextbox"));
+		search.sendKeys(s);
+		search.sendKeys(Keys.RETURN);
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		search.clear();
+		//driver.navigate().to("https://www.google.com");
+		}
+	}
+	
+	
+	//@AfterClass
+	@AfterTest
 	public void tearDown() {
 		driver.close();
 	}
