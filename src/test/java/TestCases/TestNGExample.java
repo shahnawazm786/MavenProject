8e5a1ad9c6ffa41d492f5cb98c6f2a6ef3a50600
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-19 12:05:10 +0530

    Update TestNGExample.java

--- src/test/java/TestCases/TestNGExample.java
+++ src/test/java/TestCases/TestNGExample.java
@@ -1,12 +1,32 @@
 package TestCases;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestNGExample {
-	@Test
+	WebDriver driver;
+	@BeforeClass
 	public void Method1() {
-		System.out.println("Method-01");
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver=new ChromeDriver();
 	}
-	
+	@Test
+	public void Method2() {
+		
+		driver.get("https://www.google.com");
+		
+	}
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+	}
 
 }
