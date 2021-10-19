7502d9148b04e01c77e22e01ef34caf0499f78bc
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-19 12:47:15 +0530

    TestNG
    TestNG

--- src/test/java/TestCases/TestNGExample.java
+++ src/test/java/TestCases/TestNGExample.java
@@ -1,27 +1,49 @@
 package TestCases;
 
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestNGExample {
-	WebDriver driver;
+	private WebDriver driver=null;
 	@BeforeClass
-	public void Method1() {
+	public void setup() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		driver.manage().deleteAllCookies();
-		driver=new ChromeDriver();
+		
+		
 	}
 	@Test
-	public void Method2() {
+	public void SearchTest() throws Exception {
 		
 		driver.get("https://www.google.com");
+		WebElement search=driver.findElement(By.xpath("//input[@name='q']"));
+		search.sendKeys("Selenium");
+		search.sendKeys(Keys.RETURN);
+		
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+		List<WebElement> listElement=driver.findElements(By.xpath("//h3[@class='LC20lb DKV0Md']"));
+		System.out.println(listElement.size());
+		if(listElement.size()>0) {
+			assertTrue(true,"List found of "+listElement.size());
+		}
+		else {
+			assertFalse(true);
+		}
 		
 	}
 	@AfterClass
