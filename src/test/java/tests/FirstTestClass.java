3d8d4d4818b05aea577a6d29b649f3a933ed3319
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-20 15:30:05 +0530

    testng file
    testng file

---
+++ src/test/java/tests/FirstTestClass.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class FirstTestClass {
+	@Test
+	public void testMethod1() {
+		System.out.println("First test method-01");
+	}
+	@Test
+	public void testMethod2() {
+		System.out.println("First test method-02");	
+	}
+}
---
+++ src/test/java/tests/SecondTestClass.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class SecondTestClass {
+	@Test
+	public void testMethod3() {
+		System.out.println("First test method-03");
+	}
+	@Test
+	public void testMethod4() {
+		System.out.println("First test method-04");	
+	}
+}
