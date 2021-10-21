3f8e23836a102fd828a995907eb31d309bf296a2
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-21 14:40:19 +0530

    Update TestClasses.java

--- src/test/java/tests/beforeafter/TestClasses.java
+++ src/test/java/tests/beforeafter/TestClasses.java
@@ -1,7 +1,10 @@
 package tests.beforeafter;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class TestClasses extends BaseClass{
 	@BeforeClass
@@ -11,6 +14,18 @@ public class TestClasses extends BaseClass{
 	@AfterClass
 	public void afterChildClass() {
 		System.out.println("Child After Class Method");
+	}
+	@BeforeMethod
+	public void beforeChildMethod() {
+		System.out.println("Child Before Method");
+	}
+	@AfterMethod
+	public void afterChildMethod() {
+		System.out.println("Child After Method");
+	}
+	@Test
+	public void testMethod() {
+		System.out.println("Test Method under TestClasses");
 	}
 	
 
