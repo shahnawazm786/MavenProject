e6e0f050fbee5c22768ed093ef6d6136608a53a5
Shahnawaz <49604292+shahnawazm786@users.noreply.github.com>
2021-10-27 11:12:51 +0530

    css selector
    css selector

--- src/test/java/javascript/JavaScriptExecutorExample.java
+++ src/test/java/javascript/JavaScriptExecutorExample.java
@@ -1,7 +1,12 @@
 package javascript;
 
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -16,6 +21,21 @@ public class JavaScriptExecutorExample {
 		driver.get("https://www.browserstack.com");
 		js=(JavascriptExecutor)driver;
 		js.executeAsyncScript("window.scrollBy(0,document.body.scrollHeight)");
+		driver.close();
 	}
+	public void methodCSSSelector() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://blog.testproject.io/");
+		//js=(JavascriptExecutor)driver;
+		//js.executeAsyncScript("window.scrollBy(0,document.body.scrollHeight)");
+		WebElement ele=driver.findElement(By.cssSelector("#menu-top-menu-1"));
+		if(ele!=null)
+			assertTrue(true,"Element found");
+		else
+			assertFalse(true);
+		
 
+	}
 }
